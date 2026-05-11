83cc7926816d54cba03400a0668ec2923367dca4
Reuben Kitaka Maithya ⁠ <reukitaka@gmail.com>
2026-05-11 20:00:43 +0300

    chore: initial project setup with Spring Boot 3, Java 21

---
+++ src/test/java/com/example/hazina/HazinaApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.hazina;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HazinaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
